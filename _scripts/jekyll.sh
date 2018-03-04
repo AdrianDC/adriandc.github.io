0ff2df8940ac6c089b2ee399550fa47953980fe1
Adrian DC <radian.dc@gmail.com>
2018-03-04 22:38:24 +0100

    jekyll: Add initial local instantiation script
     * Based upon shtoolsdocs from :
        http://adriandc.github.io/android_development_shell_tools/#projectdocsrc
    
    Change-Id: I18afb5b9a770160a58702483f302b55c9eab4b75
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

---
+++ _scripts/jekyll.sh
@@ -0,0 +1,16 @@
+# Installation mode
+if [ ! -z "${1}" ]; then
+  sudo apt install build-essential curl libcurl4-openssl-dev libffi-dev ruby-dev zlib1g-dev;
+  sudo gem install rubygems-update;
+  sudo update_rubygems;
+  sudo gem update --system;
+  sudo gem install bundler;
+  sudo bundle install;
+  bundle;
+fi;
+
+# Launch Jekyll
+bundle exec jekyll serve --host 0.0.0.0 --incremental --port 4003;
+
+# Cleanup server files
+rm -rf ./_site ./.jekyll-metadata ./Gemfile.lock;
